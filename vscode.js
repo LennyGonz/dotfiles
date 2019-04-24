601ec7bf361fa3421985aaf20379c9540eba7ea1
Lenny Gonzalez <lenny.gonzaleznyc@gmail.com>
2019-04-23 22:33:53 -0400

    My VSCode settings

---
+++ vscode.js
@@ -0,0 +1,26 @@
+{
+  "workbench.iconTheme": "material-icon-theme",
+  "workbench.colorTheme": "Material Theme Ocean High Contrast",
+  "workbench.activityBar.visible": true,
+  "editor.cursorBlinking": "expand",
+  "editor.fontFamily": "'Dank Mono'",
+  "editor.fontWeight": "400",
+  "editor.tabSize": 2,
+  "editor.wordWrap": "on",
+  "editor.matchBrackets": true,
+  "terminal.integrated.fontFamily": "'Dank Mono'",
+  "terminal.integrated.fontSize": 18,
+  "editor.fontSize": 18,
+  "editor.minimap.enabled": false,
+  "editor.formatOnSave": true,
+  "[javascript]": {
+    "editor.formatOnSave": false
+  },
+  "prettier.disableLanguages": ["js"],
+  // tell the ESLint plugin to run on save
+  "eslint.autoFixOnSave": true,
+  "files.insertFinalNewline": true,
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact"
+  }
+}
